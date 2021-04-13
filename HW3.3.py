84b152100ec7f4c2648fb5a1fc369ad49857741f
Gali <melonruletheworld@gmail.com>
2021-04-13 18:13:44 +0200

    Third HW

--- HW3.3.py
+++ HW3.3.py
@@ -1,2 +1,7 @@
 # Реализовать функцию my_func(), которая принимает три позиционных аргумента,
 # и возвращает сумму наибольших двух аргументов.
+
+def my_func(a, b, c):
+        return min(a, b, c)
+
+print(my_func(input("Insert 1.arg: "), input("Insert 2.arg: "), input("Insert 3.arg: ")))
--- HW3.6.py
+++ HW3.6.py
@@ -2,5 +2,5 @@
 # но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
 
 def int_func(a):
-    return str.capitalize(a)
+    return str.title(a)
 print(int_func(a = input('Insert lowercase text -> ')))

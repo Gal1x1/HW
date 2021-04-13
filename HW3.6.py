b56f30eaba37b70d9e4026425867b10623612188
Gali <melonruletheworld@gmail.com>
2021-04-13 14:09:30 +0200

    Third HW

--- HW3.6.py
+++ HW3.6.py
@@ -1,4 +1,6 @@
 # Реализовать функцию int_func(), принимающую слово из маленьких латинских букв и возвращающую его же,
 # но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
 
-print(flush)
+def int_func(a):
+    return str.capitalize(a)
+print(int_func(a = input('Insert lowercase text -> ')))
